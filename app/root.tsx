22272b7cf902ff60510aa99047e5d8efcf5ebfcc
cowboi <miraclecowboy@outlook.com>
2026-02-23 12:09:48 +0800

    Just Good Friends

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="zh-CN">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=1200, initial-scale=1" />
         <Meta />
         <Links />
       </head>
